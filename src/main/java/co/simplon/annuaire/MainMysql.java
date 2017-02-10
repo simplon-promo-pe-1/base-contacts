2d0f7406a0fad70cdca0b91c2b75f6c1cfd9e0e6
Eric Siber <eric.siber@gmail.com>
2017-02-10 10:14:34 +0100

    étape 4

---
+++ src/main/java/co/simplon/annuaire/MainMysql.java
@@ -0,0 +1,29 @@
+package co.simplon.annuaire;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import com.mysql.jdbc.Driver;
+
+public class MainMysql {
+
+    static String databaseUrl = "jdbc:mysql://HOSTNAME:3306/DBID?user=user&password=password";
+    static String requeteSql = "SELECT FNAME FROM CONTACTS ORDER BY FNAME";
+
+    public static void main(String[] args) throws Exception {
+        DriverManager.registerDriver(new Driver());
+
+        Connection connexion = DriverManager.getConnection(databaseUrl);
+        Statement requete = connexion.createStatement();
+        ResultSet resultat = requete.executeQuery(requeteSql);
+        while (resultat.next()) {
+            String prenom = resultat.getString("FNAME");
+            System.out.println(prenom);
+        }
+        resultat.close();
+        requete.close();
+        connexion.close();
+    }
+}
