2b2658fb19a5af1b140cb11515bcf982ce16a121
Eric Siber <eric.siber@gmail.com>
2017-02-10 10:46:42 +0100

    étape 8

--- src/main/java/co/simplon/annuaire/Main.java
+++ src/main/java/co/simplon/annuaire/Main.java
@@ -9,15 +9,20 @@ import oracle.jdbc.OracleDriver;
 
 public class Main {
 
-    static String databaseUrl = "jdbc:oracle:thin:localhost:1521:XE";
-    static String requeteSql = "SELECT EMAIL FROM CONTACTS WHERE CONTACT_TYPE='friend' ";
-
+	static String databaseUrl = "jdbc:oracle:thin:localhost:1521:XE";
+    static String requeteSqlPrefix = "SELECT EMAIL FROM CONTACTS WHERE CONTACT_TYPE='";
+    static String requeteSqlSuffixe = "' ";
+    
     public static void main(String[] args) throws Exception {
+    		String type = "";
+    		if (args.length > 0) {
+    			type = args[0];
+    		};
         DriverManager.registerDriver(new OracleDriver());
 
         Connection connexion = DriverManager.getConnection(databaseUrl, "SIMPLON", "SIMPLON");
         Statement requete = connexion.createStatement();
-        ResultSet resultat = requete.executeQuery(requeteSql);
+        ResultSet resultat = requete.executeQuery(requeteSqlPrefix + type + requeteSqlSuffixe);
         while (resultat.next()) {
             String email = resultat.getString(1);
             System.out.println(email);
