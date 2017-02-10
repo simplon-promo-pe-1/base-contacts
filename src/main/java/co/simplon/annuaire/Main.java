07f640273ece51ce5a8fea0466d50bd7b7c338e7
Eric Siber <eric.siber@gmail.com>
2017-02-10 09:57:46 +0100

    étape 1

---
+++ src/main/java/co/simplon/annuaire/Main.java
@@ -0,0 +1,29 @@
+package co.simplon.annuaire;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import oracle.jdbc.OracleDriver;
+
+public class Main {
+
+    static String databaseUrl = "jdbc:oracle:thin:SIMPLON/SIMPLON@localhost:1521:XE";
+    static String requeteSql = "SELECT FNAME FROM CONTACTS";
+
+    public static void main(String[] args) throws Exception {
+        DriverManager.registerDriver(new OracleDriver());
+
+        Connection connexion = DriverManager.getConnection(databaseUrl);
+        Statement requete = connexion.createStatement();
+        ResultSet resultat = requete.executeQuery(requeteSql);
+        while (resultat.next()) {
+            String prenom = resultat.getString("FNAME");
+            System.out.println(prenom);
+        }
+        resultat.close();
+        requete.close();
+        connexion.close();
+    }
+}
