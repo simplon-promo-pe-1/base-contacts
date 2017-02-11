b44c1f37448ab9ecf365a9998815de7d2e3db43e
Eric Siber <eric.siber@gmail.com>
2017-02-11 06:28:47 +0100

    formattage

--- src/main/java/co/simplon/annuaire/Main.java
+++ src/main/java/co/simplon/annuaire/Main.java
@@ -7,18 +7,18 @@ import co.simplon.annuaire.dao.JdbcContactsDAO;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-    		String type = "";
-    		if (args.length > 0) {
-    			type = args[0];
-    		};
-    		
-    		ContactsDAO dao = new JdbcContactsDAO();
-        List<String> listeEmails = dao.findEmailsByContactType(type);
-        
-        for (String email : listeEmails) {
-        		System.out.println(email);
+	public static void main(String[] args) throws Exception {
+		String type = "";
+		if (args.length > 0) {
+			type = args[0];
 		}
-    }
+
+		ContactsDAO dao = new JdbcContactsDAO();
+		List<String> listeEmails = dao.findEmailsByContactType(type);
+
+		for (String email : listeEmails) {
+			System.out.println(email);
+		}
+	}
 
 }
--- src/main/java/co/simplon/annuaire/dao/AbstractJdbcContactDAO.java
+++ src/main/java/co/simplon/annuaire/dao/AbstractJdbcContactDAO.java
@@ -12,35 +12,39 @@ import java.util.List;
 public abstract class AbstractJdbcContactDAO implements ContactsDAO {
 
 	static String requeteSql = "SELECT EMAIL FROM CONTACTS WHERE CONTACT_TYPE = ? ";
-	
+
 	public abstract String getUrl();
-	
+
 	public abstract Driver getDriver() throws SQLException;
 
 	public AbstractJdbcContactDAO() {
 		super();
 	}
 
-    /* (non-Javadoc)
-	 * @see co.simplon.annuaire.dao.ContactsDAO#findEmailsByContactType(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * co.simplon.annuaire.dao.ContactsDAO#findEmailsByContactType(java.lang
+	 * .String)
 	 */
 	@Override
 	public List<String> findEmailsByContactType(String type) throws Exception {
-				List<String> emails = new ArrayList<String>();
-				DriverManager.registerDriver(getDriver());
-			
-			    Connection connexion = DriverManager.getConnection(getUrl());
-			    PreparedStatement requete = connexion.prepareStatement(requeteSql);
-			    requete.setString(1, type);
-			    ResultSet resultat = requete.executeQuery();
-			    while (resultat.next()) {
-			        String email = resultat.getString(1);
-			        emails.add(email);
-			    }
-			    resultat.close();
-			    requete.close();
-			    connexion.close();
-			    return emails;
-			}
+		List<String> emails = new ArrayList<String>();
+		DriverManager.registerDriver(getDriver());
+
+		Connection connexion = DriverManager.getConnection(getUrl());
+		PreparedStatement requete = connexion.prepareStatement(requeteSql);
+		requete.setString(1, type);
+		ResultSet resultat = requete.executeQuery();
+		while (resultat.next()) {
+			String email = resultat.getString(1);
+			emails.add(email);
+		}
+		resultat.close();
+		requete.close();
+		connexion.close();
+		return emails;
+	}
 
 }
